e72c6e34360b731d3a8f6465d9d149d0191ab8f7
Vanessa <vanessatellez45@gmail.com>
2020-01-17 15:30:35 -0500

    mvp

--- data/routers/actionRouter.js
+++ data/routers/actionRouter.js
@@ -4,4 +4,68 @@ const Action = require('../helpers/actionModel');
 
 const router = express.Router();
 
+// GET all actions -- /api/action
+router.get('/', (req, res) => {
+    Action.get()
+    .then(action => {
+        res.status(200).json(action);
+    })
+    .catch(error =>{
+        console.log(error);
+        res.status(500).json({
+            error: "The action could not be retrieved."
+        });
+    });
+});
+
+
+// DELETE specific action -- /api/action/:id
+router.delete('/:id', (req, res) =>{
+    Action.remove(req.params.id)
+    .then(deleted => {
+        res.status(204).json(deleted);
+    })
+    .catch(error =>{
+        console.log(error);
+        res.status(500).json({
+            error: "The action could not be removed"
+        });
+    });
+});
+
+// PUT - UPDATE specific action -- /api/action/:id
+router.put('/:id', (req, res) => {
+const id = req.params.id;
+const { notes, description } = req.body;
+
+if(!notes || !description){
+    res.status(400).json({
+        errorMessage: "Please provide name and description for the project."
+    });
+}
+
+    Action.update(id, req.body)
+    .then(updatedProject =>{
+        res.status(200).json(updatedProject);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            error: "The project could not be updated."
+        });
+    });
+});
+
+//POST - Insert new action -- /api/action
+router.post('/', (req, res) => {
+    Action.insert(req.body)
+    .then(newAction => {
+    res.status(201).json(newAction)
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({ error: "Cannot insert action."})
+    });
+});
+
 module.exports = router;
--- data/routers/projectRouter.js
+++ data/routers/projectRouter.js
@@ -4,7 +4,111 @@ const Project = require('../helpers/projectModel');
 
 const router = express.Router();
 
+//Insert new project -- /api/project
+router.post("/", (req, res) => {
+    const project = req.body;
 
+    Project.insert(project)
+    .then(newProject =>{
+        res.status(201).json(newProject)
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            error: "There was an error while saving project to the database."
+        });
+    });
+});
+
+
+// GET all projects -- /api/project
+router.get('/', (req, res) => {
+    Project.get()
+    .then(project => {
+        res.status(200).json(project);
+    })
+    .catch(error => {
+        console.log('error from GET request', error)
+        res.status(500).json({ errorMessage: 'Retrieving project information failed'})
+    })
+})
+
+// GET specific project -- /api/project/:id
+router.get('/:id', validateProjectId, (req, res) => {
+    Project.get(req.param.id)
+    .then(project =>{
+        res.status(200).json(project);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            error: "Project info could not be retrieved."
+        });
+    });
+});
+
+// GET specific project with actions
+router.get('/:id/actions', validateProjectId, (req, res) => {
+    Project.getProjectActions(req.params.id)
+    .then(projActions =>{
+        res.status(200).json(projActions);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({ error: "Cannot get actions for project."})
+    });
+});
+
+// DELETE specific project -- /api/project/:id
+router.delete('/:id', validateProjectId, (req, res) =>{
+    Project.remove(req.params.id)
+    .then(deleted => {
+        res.status(204).json(deleted);
+    })
+    .catch(error =>{
+        console.log(error);
+        res.status(500).json({
+            error: "The project could not be removed"
+        });
+    });
+});
+
+// UPDATE specific project -- /api/project/:id
+router.put('/:id', validateProjectId, (req, res) => {
+const id = req.params.id;
+const { name, description } = req.body;
+
+    if(!name || !description){
+        res.status(400).json({
+            errorMessage: "Please provide name and description for the project."
+        })
+    }
+
+    Project.update(id, req.body)
+    .then(updatedProject =>{
+        res.status(200).json(updatedProject);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            error: "The project could not be updated."
+        });
+    });
+});
+
+
+//Middleware
+function validateProjectId(req, res, next){
+    Project.get(req.params.id)
+    .then(projectId =>{
+        if(projectId){
+            req.project = projectId;
+            next();
+        } else {
+            res.status(400).json({message: "invalid project id"});
+        }
+    })
+}
 
 
 module.exports = router;
